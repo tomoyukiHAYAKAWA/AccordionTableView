a8294d2d7dca415f187b53e7a163b6571186d9f1
tomoyukiHAYAKAWA <tomotarosu13@gmail.com>
2023-01-08 01:32:11 +0900

    fix:datasourceをVC側で持つ

--- AccordionTableView/ViewController.swift
+++ AccordionTableView/ViewController.swift
@@ -7,9 +7,29 @@ final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.dataSource = viewModel
+        tableView.dataSource = self
         tableView.register(UINib(nibName: "TitleCell", bundle: nil), forCellReuseIdentifier: "TitleCell")
         tableView.register(UINib(nibName: "UserNameCell", bundle: nil), forCellReuseIdentifier: "UserNameCell")
     }
 
+}
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.cells.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch viewModel.cells[indexPath.row] {
+        case .title:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "TitleCell", for: indexPath) as? TitleCell else { return UITableViewCell() }
+            return cell
+        case .userName:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserNameCell", for: indexPath) as? UserNameCell else { return UITableViewCell() }
+            let params = UserNameCell.Params(name: viewModel.name, age: viewModel.age)
+            cell.configure(params: params)
+            return cell
+        }
+    }
 }
--- AccordionTableView/ViewModel.swift
+++ AccordionTableView/ViewModel.swift
@@ -6,28 +6,8 @@ final class ViewModel: NSObject {
         case userName
     }
 
-    private var cells: [Row] = [.title, .userName]
-    private var name: String = "ティム・クック"
-    private var age: Int = 35
+    private(set) var cells: [Row] = [.title, .userName]
+    private(set) var name: String = "ティム・クック"
+    private(set) var age: Int = 35
 
-}
-
-// MARK: - UITableViewDataSource
-extension ViewModel: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cells.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch cells[indexPath.row] {
-        case .title:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "TitleCell", for: indexPath) as? TitleCell else { return UITableViewCell() }
-            return cell
-        case .userName:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserNameCell", for: indexPath) as? UserNameCell else { return UITableViewCell() }
-            let params = UserNameCell.Params(name: name, age: age)
-            cell.configure(params: params)
-            return cell
-        }
-    }
 }
